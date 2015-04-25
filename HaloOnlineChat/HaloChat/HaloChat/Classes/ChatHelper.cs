d478fb7b0f6ad2aa8be6c11f7c9fe3c67380e446
Prom3theu5 <dave.sekula@gmail.com>
2015-04-25 11:17:52 +0100

    Threading Exception - Needed to use a delegate to update the main UI (cross threaded).

--- HaloOnlineChat/HaloChat/HaloChat/Classes/ChatHelper.cs
+++ HaloOnlineChat/HaloChat/HaloChat/Classes/ChatHelper.cs
@@ -1,5 +1,4 @@
-﻿using Guacamole.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -7,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HaloChat.Classes;
 
 namespace HaloChat.Classes
 {
